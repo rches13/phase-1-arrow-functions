ee00958b5d0c6bf6500484b498a1af9f7e24f17b
Rodgers Chesoni <rodgerschesoni@gmail.com>
2024-07-12 16:57:19 +0300

    adds arrow functions

---
+++ index.js
@@ -0,0 +1,14 @@
+// Function expression for divide
+const divide = function(a = 2000, b = 100) {
+    return a / b;
+}
+
+// Arrow function for square
+const square = (x) => {
+    return x * x;
+}
+
+// Arrow function for add
+const add = (a, b) => {
+    return a + b;
+}
